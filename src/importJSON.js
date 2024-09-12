0060046aaca11b20041ee8b0a717fdc7a5b0b2fb
Han Wang <hanforwork896@gmail.com>
2024-09-13 02:11:19 +0800

    Day03 - tsconfig.json

---
+++ src/importJSON.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const data_json_1 = __importDefault(require("./JSON/data.json"));
+console.log(data_json_1.default.name);
+console.log(data_json_1.default.type);
+//# sourceMappingURL=importJSON.js.map
---
+++ src/importJSON.ts
@@ -0,0 +1,4 @@
+import data from './JSON/data.json';
+
+console.log(data.name);  // Hannah
+console.log(data.type);  // Human
