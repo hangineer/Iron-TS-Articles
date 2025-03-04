28c61f54354139399afd6c5e579624cdb78a972c
Hannah ʕ•ᴥ•ʔ <99162505+hangineer@users.noreply.github.com>
2025-03-04 15:45:10 +0800

    Update Day9 example.ts

--- src/Day09(Function Overloads)/example.ts
+++ src/Day09(Function Overloads)/example.ts
@@ -4,15 +4,14 @@ function greet(age: number): string;
 // function greet(id: number,): string;
 
 // 實施簽名 Implementation Signature
-function greet(value: any): string {
+function greet(value: string | number): string {
     if (typeof value === "string") {
         return `Hello, ${value}!`;
-    } else if (typeof value === "number") {
+    } else {
         return `You are ${value} years old.`;
     }
-    return "Invalid type";
 }
 
 
-console.log(greet("Alice"));  // Hello, Alice!
-console.log(greet(25));       // You are 25 years old.
+console.log(greet("Alice"));  // ✅ Hello, Alice!
+console.log(greet(25));       // ✅ You are 25 years old.
